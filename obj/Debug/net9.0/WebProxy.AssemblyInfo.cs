5679d3ee2c1a08017487a0099cb31fd2c680698f
Khanh Tra Nguyen Tran <faynguyen03@Khanhs-MacBook-Pro.local>
2025-02-23 17:22:55 +0000

    Updated

--- obj/Debug/net9.0/WebProxy.AssemblyInfo.cs
+++ obj/Debug/net9.0/WebProxy.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebProxy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4e9ede3030e0586169826bdb84cec965a152e68")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36cdad006697c53e31b70d8cd7abec4ac7175562")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebProxy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebProxy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
